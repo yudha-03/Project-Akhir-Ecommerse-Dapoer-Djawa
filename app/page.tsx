76cc1dedd9f50a130da2505438bee1015bdf8dff
unknown <satriapradanaryudha@gmail.com>
2026-06-08 11:14:27 +0800

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Dapoer Djawa
           </a>
         </div>
       </main>

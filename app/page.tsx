481e57d2b1b8d83968588670a33df6af8e68e40d
Your Name <rahmatrohmatdoni@gmail.com>
2026-06-15 11:04:34 +0800

    Testing

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapoer Djawa
+            Testing
           </a>
         </div>
       </main>
